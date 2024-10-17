ece2d3cbea71f2095ebf9d1e7fe4b2776fdeda19
haitd79 <dhait@outlook.com>
2024-10-17 11:01:13 -0600

    items and textures
    Added a new reusable item object that can easily be changed and placed around the scene
    added new new textures for enemies and items
    implemented boot item for unlocking dash

--- cmpt230-game/Assets/Scenes/Levels/GameData.gd
+++ cmpt230-game/Assets/Scenes/Levels/GameData.gd
@@ -14,5 +14,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_item_collected() -> void:
+func _on_item_boot_collected() -> void:
 	emit_signal("dash_collect")
+
+
+func _on_item_health_collected() -> void:
+	print("nice health, but we aint implemented that yet")
+
+
+func _on_item_whip_collected() -> void:
+	print("nice whip, but we aint implemented that yet")
